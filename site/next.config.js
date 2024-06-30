c334bea5fe45f4ec17adcb3b82e4cd4a131769d0
heulenderwolf <96176849+heulenderwolf@users.noreply.github.com>
2024-06-30 20:19:10 +0200

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'tobimobi',
+    description: 'My store for my workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://heulenderwolf.github.io/kasm-registry/',
+    contactUrl: 'https://github.com/heulenderwolf/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
